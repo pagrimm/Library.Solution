b95531cfb37f256ec08f25303a3ab5053393ac20
Ian Scott <chesnekov@gmail.com>
2020-08-12 11:31:31 -0700

    adds links to Author and Book details pages which give access to adding Book/Author relationships respectively. Also fixes error in AuthorsController which was preventing drop down list of Books from populating
    Co-authored-by: Peter Grimm <pagrimm@gmail.com>
    Co-authored-by: Brittany Lindgren <brittdevelopsit@gmail.com>

---
+++ Library/Migrations/20200812181820_CopyUserV3.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Library.Migrations
+{
+    public partial class CopyUserV3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Copies_AspNetUsers_UserId",
+                table: "Copies");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Copies_UserId",
+                table: "Copies");
+
+            migrationBuilder.DropColumn(
+                name: "CheckoutDate",
+                table: "Copies");
+
+            migrationBuilder.DropColumn(
+                name: "DueDate",
+                table: "Copies");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Copies");
+
+            migrationBuilder.CreateTable(
+                name: "CopyApplicationUser",
+                columns: table => new
+                {
+                    CopyApplicationUserId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    CopyId = table.Column<int>(nullable: false),
+                    ApplicationUserId = table.Column<string>(nullable: true),
+                    CheckoutDate = table.Column<DateTime>(nullable: false),
+                    DueDate = table.Column<DateTime>(nullable: false),
+                    Returned = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CopyApplicationUser", x => x.CopyApplicationUserId);
+                    table.ForeignKey(
+                        name: "FK_CopyApplicationUser_AspNetUsers_ApplicationUserId",
+                        column: x => x.ApplicationUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_CopyApplicationUser_Copies_CopyId",
+                        column: x => x.CopyId,
+                        principalTable: "Copies",
+                        principalColumn: "CopyId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CopyApplicationUser_ApplicationUserId",
+                table: "CopyApplicationUser",
+                column: "ApplicationUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CopyApplicationUser_CopyId",
+                table: "CopyApplicationUser",
+                column: "CopyId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CopyApplicationUser");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CheckoutDate",
+                table: "Copies",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DueDate",
+                table: "Copies",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Copies",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Copies_UserId",
+                table: "Copies",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Copies_AspNetUsers_UserId",
+                table: "Copies",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
